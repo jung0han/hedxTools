1f498da8834b3419554e2c9d73f55f71cdc8c4f1
정동우 <dongwoo.jeong@lge.com>
2021-03-29 11:34:55 +0900

    Add queryFieldData function

--- R/query.R
+++ R/query.R
@@ -1,142 +1,169 @@
-library(DBI)
-library(RMySQL)
+#' query field data from DB
+#'
+#' Field 불량율을 계산하는 함수
+#'
+#' @param date 검색 기준일, Datetime(yyyy-mm-dd)
+#' @param period 검색 기간(년), Int
+#' @param type 생산/판매 법인 기준 선택, prod or sales
+#' @param cal 계산 기준, ffr or fdr
+#' @param accNum 누적 기간으로 fdr를 계산 시 사용, Int
+#' @param svcFilters SVC 데이터 쿼리의 Where문에 삽인되는 필터, list()
+#' @param sellinFilters Sellin 데이터 쿼리의 Where문에 삽인되는 필터, list()
+#' @return svc 수량, sales 수량, 불량율
+#'
+#' @rdname queryFieldData
+#' @importFrom magrittr %>%
+#' @export
 
-# 데이터베이스 연결하기 (updated)
-con = dbConnect(RMySQL::MySQL(),
-                user='cs_task_shiny',
-                password='lge123!!',
-                dbname='cs_task_he',
-                host='10.185.152.89',
-                port = 3306,
-                client.flag = CLIENT_MULTI_RESULTS)
+queryFieldData <- function(date, period = 2, type = "prod", cal = "ffr", accNum = 6, svcFilters, sellinFilters) {
+        # Utility function ----------------------------------------
 
-# DB character set을 utf-8 로 지정
-dbSendQuery(con, 'set character set "utf8"')
+        # apply 함수에 사용할 월 뺄셈 함수, 지정된 월에서 해당 월만큼 뺀 후 YYYYmm형태로 출력
+        month_subtract <- function(x, month) {
+                result <- as.Date(paste0(month, 1), format="%Y%m%d") - months(x)
+                return(format(result, format = "%Y%m"))
+        }
+        # 누적 서비스 건수 계산
+        cal_acc_svc <- function(df, endDate, period, accNum = 12) {
+                months <- sapply(0:(period*12), month_subtract, month = format(endDate, format="%Y%m"))
 
-# filter mapping table 불러오는 코드
-# ref_he_tv_filter <- dbGetQuery(con, "SELECT * FROM `ref_he_tv_filter_value`;")
-# 
-# filter_names <- unique(ref_he_tv_filter$COL_NAME)
-# 
-# for(filter_name in filter_names) {
-#         filters_list[[filter_name]] <- ref_he_tv_filter$VALUE1[ref_he_tv_filter$COL_NAME == filter_name]
-# }
+                acc_svc <- c()
+                for(month in months) {
+                        acc_months <- sapply(0:(accNum-1), month_subtract, month = month)
+                        
+                        acc_svc[month] <- df %>% dplyr::filter(
+                                ISSUE_MON %in% acc_months,
+                                PURC_MON_NEW %in% acc_months
+                        ) %>% dplyr::select(SVC_COUNT) %>% colSums()
+                }
+                return(acc_svc)
+        }
+        # 누적 판매 수량 계산
+        cal_acc_sales <- function(df, endDate, period, accNum = 12) {
+                months <- sapply(0:(period*12), month_subtract, month = format(endDate, format="%Y%m"))
 
-# prod or sales 중 선택
-type <- "prod"
+                acc_sales <- c()
+                for(month in months) {
+                        acc_months <- sapply(0:(accNum-1), month_subtract, month = month)
+                        acc_sales[month] <- df %>% dplyr::filter(
+                                PURC_MON %in% acc_months
+                        ) %>% dplyr::select(SELLIN_SUM) %>% colSums()
+                }
+                return(acc_sales)
+        }
+        # 가중치를 반영한 누적 판매 수량 계산
+        cal_w.acc_sales <- function(df, endDate, period, accNum = 12) {
+                months <- sapply(0:(period*12), month_subtract, month = format(endDate, format="%Y%m"))
 
-# filter 변수 예시, shiny에서 list로 출력하고 각 문자열은 작은 따옴표로 묶여있어야 함
-filters_svc <- list()
-filters_svc[["PRODUCT_DIVIDE"]] <- c("'LED_UHD'", "'LED_FHD'", "'OLED'")
-filters_svc[["LEVEL1_L12M"]] <- c("'01.모듈'")
-filters_svc[["SALES_CORP"]] <- c("'LGEIN'")
-filters_svc[["PROD_CORP"]] <- c("")
-filters_svc[["CONTINENT_E"]] <- c("")
-filters_svc[["TOOL4"]] <- c("")
-filters_svc[["ATTRIBUTE6"]] <- c("")
-#filters_svc[["DEV_YEAR"]] <- c("'18년'", "'19년'")
-#filters_svc[["INCH"]] <- c("'55'", "'65'")
+                w.acc_sales <- c()
+                for(month in months) {
+                        acc_months <- sapply(0:(accNum-1), month_subtract, month = month)
+                        w_sales <- 0
+                        for(index in 1:accNum) {
+                                w_sales <- w_sales + df %>% dplyr::filter(
+                                        PURC_MON == acc_months[index]
+                                ) %>% 
+                                        dplyr::select(SELLIN_SUM) %>% colSums() * (index / accNum)
+                        }
+                        w.acc_sales[month] <- w_sales
+                }
+                return(w.acc_sales)
+        }
 
-# filter 변수가 적용되지 않은 리스트를 제외
-filters_svc <- filters_svc[lapply(filters_svc, function(x) nchar(x[1])) > 0]
+        # Main function ----------------------------------------
 
-# sellin filter 변수 예시
-cols_sellin <- c("SALES_CORP", "PROD_CORP", "CONTINENT_E", "TOOL4", "ATTRIBUTE6")
-filters_sellin <- list()
-for(colname in cols_sellin) {
-        filters_sellin[[colname]] <- filters_svc[[colname]]
-}
+        end_date <- as.Date(date)
+        start_date <- end_date - years(period + 1) - 1
 
-# shiny로 시작일 입력 받음
-end_date <- as.Date("2021-03-01")
-start_date <- end_date - years(3) - 1
+        start_month <- format(start_date, format="%Y%m")
+        end_month <- format(end_date, format="%Y%m")
+        end_day <- format(end_date, format="%Y%m%d")
 
+        # 입력 받은 필터로 쿼리문을 만듬
+        query_filters_svc <- ""
+        query_filters_sellin <- ""
+        for(colname in names(svcFilters)) {
+                filters <- paste(svcFilters[[colname]], collapse = ",")
+                query_filters_svc <- paste(query_filters_svc, "\nAND", colname, "IN (", filters, ")")
+        }
 
-query_filters_svc <- ""
-query_filters_sellin <- ""
-for(colname in names(filters_svc)) {
-        filters <- paste(filters_svc[[colname]], collapse = ",")
-        query_filters_svc <- paste(query_filters_svc, "\nAND", colname, "IN (", filters, ")")
-}
+        for(colname in names(sellinFilters)) {
+                filters <- paste(sellinFilters[[colname]], collapse = ",")
+                query_filters_sellin <- paste(query_filters_sellin, "\nAND", colname, "IN (", filters, ")")
+        }
+        
+        # DB 커넥션 개수 확인 후 15개 이상일 경우 모두 종료
+        all_cons <- dbListConnections(MySQL())
+        message(paste("연결된 DB Connection :", length(all_cons)))
+        if(length(all_cons) > 4) {
+                for(con in all_cons)
+                        +  dbDisconnect(con)
+        }
 
-for(colname in names(filters_sellin)) {
-        filters <- paste(filters_svc[[colname]], collapse = ",")
-        query_filters_sellin <- paste(query_filters_sellin, "\nAND", colname, "IN (", filters, ")")
-}
+        # 데이터베이스 연결하기 (updated)
+        con = DBI::dbConnect(RMySQL::MySQL(),
+                        user='cs_task_shiny',
+                        password='lge123!!',
+                        dbname='cs_task_he',
+                        host='10.185.152.89',
+                        port = 3306,
+                        client.flag = CLIENT_MULTI_RESULTS)
 
-start_month <- format(start_date, format="%Y%m")
-end_month <- format(end_date, format="%Y%m")
-end_day <- format(end_date, format="%Y%m%d")
-end_year <- format(end_date, format="%Y")
+        # DB character set을 utf-8 로 지정
+        DBI::dbSendQuery(con, 'set character set "utf8"')
 
-# svc data query
-svc_query <- paste(
-"SELECT ISSUE_MON
-        ,PURC_MON_NEW
-        ,PRODUCT_DIVIDE
+        # svc data query
+        svc_query <- paste(
+        "SELECT ISSUE_MON
+                ,PURC_MON_NEW
+                ,PRODUCT_DIVIDE
+                ,PROD_CORP
+                ,SALES_CORP
+                ,LEVEL1_L12M         AS DETAIL
+                ,COUNT(PURC_MON_NEW) AS SVC_COUNT
+        FROM he_tv_svc_global_output_summary
+        WHERE ISSUE_MON >=",start_month,
+        "\nAND ISSUE_MON <=", end_month,
+        "\nAND PURC_MON_NEW >=", start_month,
+        "\nAND PURC_MON_NEW <=", end_month,
+        "\nAND DATA_DT <= ", end_day,
+        query_filters_svc, "
+        GROUP BY  ISSUE_MON
+                ,PURC_MON_NEW
+                ,SALES_CORP
+                ,PROD_CORP"
+        )
+        message(svc_query)
+
+        sellin_query <- paste(
+        "SELECT PURC_MON 
         ,PROD_CORP
         ,SALES_CORP
-        ,LEVEL1_L12M         AS DETAIL
-        ,COUNT(PURC_MON_NEW) AS SVC_COUNT
-FROM he_tv_svc_global_output_summary
-WHERE ISSUE_MON >=",start_month,
-"\nAND ISSUE_MON <=", end_month,
-"\nAND PURC_MON_NEW >=", start_month,
-"\nAND PURC_MON_NEW <=", end_month,
-"\nAND DATA_DT <= ", end_day,
-query_filters_svc, "
-GROUP BY  ISSUE_MON
-         ,PURC_MON_NEW
-         ,SALES_CORP
-         ,PROD_CORP"
-)
-message(svc_query)
+        ,SUM(QTY) AS SELLIN_SUM",
+        ifelse(type == "sales", "\nFROM `he_tv_sellin_sales_output_summary`", "\nFROM `he_tv_sellin_prod_output_summary`"),
+        "\nWHERE PURC_MON >=", start_month,
+        "\nAND PURC_MON <=", end_month,
+        "\nAND DATA_DT <=", end_day,
+        query_filters_sellin, "
+        GROUP BY  PURC_MON
+                ", ifelse(type == "sales", ",SALES_CORP", ",PROD_CORP")
+        )
+        message(sellin_query)
 
-sellin_query <- paste(
-"SELECT PURC_MON 
-       ,PROD_CORP
-       ,SALES_CORP
-       ,SUM(QTY) AS SELLIN_SUM",
-ifelse(type == "sales", "\nFROM `he_tv_sellin_sales_output_summary`", "\nFROM `he_tv_sellin_prod_output_summary`"),
-"\nWHERE PURC_MON >=", start_month,
-"\nAND PURC_MON <=", end_month,
-"\nAND DATA_DT <=", end_day,
-query_filters_sellin, "
-GROUP BY  PURC_MON
-        ", ifelse(type == "sales", ",SALES_CORP", ",PROD_CORP")
-)
-message(sellin_query)
+        svc_df <- DBI::dbGetQuery(con, svc_query)
+        sellin_df <- DBI::dbGetQuery(con, sellin_query)
 
-svc_df <- dbGetQuery(con, svc_query)
-sellin_df <- dbGetQuery(con, sellin_query)
-
-# range_month <- sapply(1:13, function(x) format(as.Date(paste0(end_year,"-10-01")) - months(x), format="%Y%m"))
-
-month_subtract <- function(x, month) {
-        result <- as.Date(paste0(month, 1), format="%Y%m%d") - months(x)
-        return(format(result, format = "%Y%m"))
+        if(cal == "ffr") {
+                acc_svc <- cal_acc_svc(svc_df, end_date, period)
+                w.acc_sales <- cal_w.acc_sales(sellin_df, end_date, period)
+                ffr_rate <- acc_svc / w.acc_sales * 100
+                return(cbind(acc_svc, w.acc_sales, ffr_rate))
+        }
+        
+        if(cal == "fdr") {
+                acc_svc <- cal_acc_svc(svc_df, end_date, period, accNum)
+                acc_sales <- cal_acc_sales(sellin_df, end_date, period, accNum)
+                fdr_rate <- acc_svc / acc_sales * 100
+                return(cbind(acc_svc, acc_sales, fdr_rate))
+        }
 }
-
-range_month <- sapply(1:13, month_subtract, month = end_month)
-
-acc_svc <- c()
-for(month in range_month) {
-        acc_svc[month] <- svc_df %>% filter(
-                ISSUE_MON <= month,
-                ISSUE_MON >= month_subtract(11, month),
-                PURC_MON_NEW <= month,
-                PURC_MON_NEW >= month_subtract(11, month)
-        ) %>% select(SVC_COUNT) %>% colSums()
-}
-print(acc_svc)
-
-w.acc_sales <- c()
-acc_sales <- c()
-for(month in range_month) {
-        acc_sales[month] <- sellin_df %>% filter(PURC_MON == month) %>% select(SELLIN_SUM) %>% colSums()
-
-}
-
-rowSums(test)
-sellin_df %>% filter(PURC_MON == "202") %>% select(SELLIN_SUM) %>% colSums()
-ifelse(test, 1, 2)
