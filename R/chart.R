6839288a0522a7c89e14cf29827e25d23bad3efc
dongwoo jeong <dongwoo.jeong@lge.com>
2021-06-24 13:46:39 +0900

    Changed left label location

--- R/chart.R
+++ R/chart.R
@@ -324,7 +324,7 @@ makeFieldChart <- function(wd = getwd(),
     dxChart <- dxChart %>% highcharter::hc_add_annotation(
       labelOptions = list(
         y = ifelse(labelLocation == "right", -10, 0),
-        x = ifelse(labelLocation == "right", -10, -60),
+        x = ifelse(labelLocation == "right", -10, -70),
         verticalAlign = "middle",
         allowOverlap = TRUE,
         overflow = 'none',
